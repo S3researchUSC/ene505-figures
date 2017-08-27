c16c49edaa23d54e7c4200574ac95eb52ec58ff7
measraim <measraim@usc.edu>
2017-08-27 16:35:55 -0700

    script designating functions for each plot type

---
+++ plotfunctions.R
@@ -0,0 +1,93 @@
+f.barplot = function(dt, xval, yval, fillval, tlab, sublab, xlab, ylab, leglab, gval, leg.ord, plot.cols){
+  ggplot(dt, aes(x = xval, y = yval, fill = fillval)) + 
+    geom_bar(stat = "identity") +
+    labs(title = tlab,
+         subtitle = sublab, 
+         x = xlab,
+         y = ylab,
+         fill = leglab) +
+    theme_ipsum_rc(grid = gval) +
+    scale_fill_manual(breaks = leg.ord, values = plot.cols) +
+    theme(plot.title = element_text(size = 25, hjust = 0.5, face = "bold"),
+          plot.subtitle = element_text(size = 14, hjust = 0.5),
+          axis.title.x = element_text(hjust = 0.5, size = 20, face = "bold"),
+          axis.title.y = element_text(hjust = 0.5, size = 20, face = "bold"),
+          axis.text.x = element_text(size = 18, face="bold"),
+          axis.text.y = element_text(size = 18, face="bold"),
+          legend.text = element_text(size = 14, face = "bold"))
+}
+
+f.areaplot = function(dt, xval, yval, fillval, tlab, sublab, xlab, ylab, leglab, gval, leg.ord, plot.cols){
+  ggplot(dt, aes(x = xval, y = yval, fill = fillval)) + 
+    geom_area(stat = "identity") +
+    labs(title = tlab,
+         subtitle = sublab, 
+         x = xlab,
+         y = ylab,
+         fill = leglab) +
+    theme_ipsum_rc(grid = gval) +
+    scale_fill_manual(breaks = leg.ord, values = plot.cols) +
+    theme(plot.title = element_text(size = 25, hjust = 0.5, face = "bold"),
+          plot.subtitle = element_text(size = 14, hjust = 0.5),
+          axis.title.x = element_text(hjust = 0.5, size = 20, face = "bold"),
+          axis.title.y = element_text(hjust = 0.5, size = 20, face = "bold"),
+          axis.text.x = element_text(size = 18, face="bold"),
+          axis.text.y = element_text(size = 18, face="bold"),
+          legend.text = element_text(size = 14, face = "bold"))
+}
+
+f.lineplot = function(dt, xval, yval, fillval, tlab, sublab, xlab, ylab, leglab, gval, leg.ord, plot.cols){
+  ggplot(dt, aes(x = xval, y = yval, color = fillval)) + 
+    geom_line(stat = "identity") +
+    labs(title = tlab,
+         subtitle = sublab, 
+         x = xlab,
+         y = ylab,
+         color = leglab) +
+    theme_ipsum_rc(grid = gval) +
+    scale_color_manual(breaks = leg.ord, values = plot.cols) +
+    theme(plot.title = element_text(size = 25, hjust = 0.5, face = "bold"),
+          plot.subtitle = element_text(size = 14, hjust = 0.5),
+          axis.title.x = element_text(hjust = 0.5, size = 20, face = "bold"),
+          axis.title.y = element_text(hjust = 0.5, size = 20, face = "bold"),
+          axis.text.x = element_text(size = 18, face="bold"),
+          axis.text.y = element_text(size = 18, face="bold"),
+          legend.text = element_text(size = 14, face = "bold"))
+}
+
+f.segplot = function(dt, xval, yval, fillval, tlab, sublab, xlab, ylab, leglab, gval, leg.ord, plot.cols){
+  ggplot(dt, aes(x = xval, y = yval, color = fillval)) + 
+    geom_line(stat = "identity") +
+    geom_point(color = "white", size = 2) +
+    geom_point(size = 1) +
+    labs(title = tlab,
+         subtitle = sublab, 
+         x = xlab,
+         y = ylab,
+         color = leglab) +
+    theme_ipsum_rc(grid = gval) +
+    scale_color_manual(breaks = leg.ord, values = plot.cols) +
+    theme(plot.title = element_text(size = 25, hjust = 0.5, face = "bold"),
+          plot.subtitle = element_text(size = 14, hjust = 0.5),
+          axis.title.x = element_text(hjust = 0.5, size = 20, face = "bold"),
+          axis.title.y = element_text(hjust = 0.5, size = 20, face = "bold"),
+          axis.text.x = element_text(size = 18, face="bold"),
+          axis.text.y = element_text(size = 18, face="bold"),
+          legend.text = element_text(size = 14, face = "bold"))
+}
+
+
+
+# dt = dt_long[ ! source %in% c("Total Renewable", "Total Primary", "Total Fossil Fuels")]
+# xval = dt[, reorder(source, value)]
+# yval = dt[, value]
+# fillval = dt[,source]
+# tlab = "2014 U.S. Annual Primary Energy Production by Source"
+# sublab = "Data: U.S. Energy Information Administration"
+# gval = "X"
+# xlab = ""
+# ylab = "Btu (Quads)"
+# leglab = ""
+# plot.cols = fuel.cols
+# 
+# f_barplot(dt, xval, yval, fillval, tlab, sublab, xlab, ylab, leglab, gval, plot.cols) + coord_flip() + scale_y_continuous(breaks = seq(0,30,5), expand = c(0,0))
