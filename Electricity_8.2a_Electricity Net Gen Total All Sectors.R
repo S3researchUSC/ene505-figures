dcce274948d048916fa684513e088209f250c453
measraim <measraim@usc.edu>
2017-09-29 14:19:55 -0700

    changed plot from annual to monthly; changed to match new data format

--- Electricity_8.2a_Electricity Net Gen Total All Sectors.R
+++ Electricity_8.2a_Electricity Net Gen Total All Sectors.R
@@ -1,17 +1,15 @@
 # Electricity_8.2a_Electricity Net Gen Total All Sectors_Script #
 
-# random change
-
 # ---------------------------------------------------------------
 # INPUT DATA ----------------------------------------------------
 # ---------------------------------------------------------------
 
 file.loc 	= '/Users/MEAS/GitHub/ene505-figures' # location of scripts
-data.loc    = '/Users/MEAS/Google Drive/TA Materials/ENE505 - Fall 2015/ENE 505 Charts' # location of data file(s)
-data.file   = 'Electricity_8.2a_Electricity Net Gen Total All Sectors.csv' # data file to be used
-out.loc   = '/Users/MEAS/Google Drive/TA Materials/ENE505 - Fall 2015/ENE 505 Charts/20170827' # location of where to save figures
+data.loc    = '/Users/MEAS/Google Drive/TA Materials/ENE505 - Fall 2015/ENE 505 Charts/data_2017' # location of data file(s)
+data.file   = 'chart10-13_data.csv' # data file to be used
+out.loc   = '/Users/MEAS/Google Drive/TA Materials/ENE505 - Fall 2015/ENE 505 Charts/20170929' # location of where to save figures
 source.cols   = c("Geothermal" = "#880015",
-                  "Solar PV" = "#ff7f27",
+                  "Solar" = "#ff7f27",
                   "Waste" = "#c3c3c3",
                   "Wood" = "#b5734e",
                   "Wind" = "#3f48cc",
@@ -41,36 +39,59 @@ source.cols   = c("Geothermal" = "#880015",
 # load data ------
   setwd(data.loc)
   dt_raw = fread(data.file, header = T)
+  
+# rename columns -----
+  colnames(dt_raw) <- c("Month",
+                        "Coal",
+                        "Petroleum",
+                        "Natural Gas",
+                        "Other Gases",
+                        "Nuclear",
+                        "Hydroelectric",
+                        "Wood",
+                        "Waste",
+                        "Geothermal",
+                        "Solar",
+                        "Wind",
+                        "Total")
 
-# get annual values only (not monthly) ----
-  dt_annual = dt_raw[ str_sub(YYYYMM, start = -2) == "13" ] # last two characters of YYYYMM should be "13"
+# change date format ----
+  dt_raw[, YYYYMM := paste0("01 ", Month)]
+  dt_raw[, YYYYMM := as.Date(YYYYMM, format = "%d %Y %B")]
+  
+# melt data table from wide to long format -----
+  dt_long = melt(dt_raw, measure.vars = colnames(dt_raw)[2:13],
+                 variable.name = "MSN", value.name = "Value")
+  
+  
+  # dt_annual = dt_raw[ str_sub(YYYYMM, start = -2) == "13" ] # last two characters of YYYYMM should be "13"
 
-# rename MSN factor levels -------
-  dt_annual[, MSN := revalue(MSN, c(CLETPUS = "Coal",
-                                    GEETPUS = "Geothermal",
-                                    HPETPUS = "Hydroelectric Pumped Storage",
-                                    HVETPUS = "Hydroelectric",
-                                    NGETPUS = "Natural Gas",
-                                    NUETPUS = "Nuclear",
-                                    OJETPUS = "Other Gases",
-                                    PAETPUS = "Petroleum",
-                                    SOETPUS = "Solar PV",
-                                    WDETPUS = "Wood",
-                                    WSETPUS = "Waste",
-                                    WYETPUS = "Wind",
-                                    ELETPUS = "Total"))]
+# # rename MSN factor levels -------
+#   dt_annual[, MSN := revalue(MSN, c(CLETPUS = "Coal",
+#                                     GEETPUS = "Geothermal",
+#                                     HPETPUS = "Hydroelectric Pumped Storage",
+#                                     HVETPUS = "Hydroelectric",
+#                                     NGETPUS = "Natural Gas",
+#                                     NUETPUS = "Nuclear",
+#                                     OJETPUS = "Other Gases",
+#                                     PAETPUS = "Petroleum",
+#                                     SOETPUS = "Solar PV",
+#                                     WDETPUS = "Wood",
+#                                     WSETPUS = "Waste",
+#                                     WYETPUS = "Wind",
+#                                     ELETPUS = "Total"))]
 
-# remove pumped storage ------
-  dt_annual = dt_annual[ ! MSN == "Hydroelectric Pumped Storage" ]
+# # remove pumped storage ------
+#   dt_annual = dt_annual[ ! MSN == "Hydroelectric Pumped Storage" ]
 
-# remove rows with data "Not Available" ------
-  dt_annual = dt_annual[ ! Value == "Not Available" ]
-
-# create column of years -----
-  dt_annual[, YYYY := as.numeric(str_sub(YYYYMM, 1, 4)) ] # only keep first four characters
-
-# make Value column numeric -----
-  dt_annual[, Value := as.numeric(Value) ]
+# # remove rows with data "Not Available" ------
+#   dt_annual = dt_annual[ ! Value == "Not Available" ]
+# 
+# # create column of years -----
+#   dt_annual[, YYYY := as.numeric(str_sub(YYYYMM, 1, 4)) ] # only keep first four characters
+# 
+# # make Value column numeric -----
+#   dt_annual[, Value := as.numeric(Value) ]
 
 # ---------------------------------------------------------------
 # FIGURES -------------------------------------------------------
@@ -80,7 +101,7 @@ setwd(out.loc)
 
 # AREA PLOT -------------
 
-  dt = dt_annual[, MSN := factor(MSN, levels = c("Total",
+  dt = copy(dt_long)[, MSN := factor(MSN, levels = c("Total",
                                                  "Wind",
                                                  "Solar PV",
                                                  "Geothermal",
@@ -92,24 +113,24 @@ setwd(out.loc)
                                                  "Natural Gas",
                                                  "Petroleum",
                                                  "Coal"))][ ! MSN %in% c("Total")] # customize reorder MSN levels for stacked area chart
-  xval = dt[, YYYY]
-  yval = dt[, Value]/1000000
+  xval = dt[, YYYYMM]
+  yval = dt[, Value]/1000
   fillval = dt[, MSN]
-  tlab = "1949 - 2014 Annual U.S. Electricity Generation By Source"
+  tlab = "1973 - 2016 U.S. Monthly Electricity Generation By Source"
   sublab = "Data: EIA Annual Energy Review"
   gval = "Y"
   xlab = NULL
-  ylab = "Kilowatt-hour (Trillions)"
+  ylab = "Kilowatt-hour (Billions)"
   leglab = ""
   leg.ord = levels(fillval)
   plot.cols = source.cols
   
-  area_elec_netgen_1949_2014 = f.areaplot(dt, xval, yval, fillval, 
+  area_elec_netgen_1973_2017 = f.areaplot(dt, xval, yval, fillval, 
                                           tlab, sublab, xlab, ylab, leglab, gval, leg.ord, plot.cols) + 
-    scale_x_continuous(breaks = seq(1945,2015,10), expand = c(0,0)) +
-    scale_y_continuous(breaks = seq(0,4.5,0.5), expand = c(0,0))
+    scale_x_date(date_breaks = "5 years", date_labels = "%Y", expand = c(0,1)) +
+    scale_y_continuous(breaks = seq(0,500,50), expand = c(0,0))
   
-  ggsave(area_elec_netgen_1949_2014, 
+  ggsave(area_elec_netgen_1973_2017, 
          filename = "Electricity_8.2a_Electricity Net Gen Total All Sectors_ATS.png", 
          width = 11.1, 
          height = 6.25, 
@@ -118,53 +139,26 @@ setwd(out.loc)
 # LINE PLOT -------------
   
   dt = dt_annual[ ! MSN %in% c("Total")]
-  xval = dt[, YYYY]
-  yval = dt[, Value]/1000000
+  xval = dt[, YYYYMM]
+  yval = dt[, Value]/1000
   fillval = dt[, MSN]
-  tlab = "1949 - 2014 Annual U.S. Electricity Generation By Source"
+  tlab = "1973 - 2017 U.S. Monthly Net Electricity Generation By Source"
   sublab = "Data: EIA Annual Energy Review"
   gval = "Y"
   xlab = NULL
-  ylab = "Kilowatt-hour (Trillions)"
+  ylab = "Kilowatt-hour (Billions)"
   leglab = ""
-  leg.ord = levels(with(dt[YYYY == "2014"], reorder(MSN, -Value))) # reorder legend of line plot to order 2014 values from least to greatest
+  leg.ord = levels(with(dt[YYYYMM == "2017-04-01"], reorder(MSN, -Value))) # reorder legend of line plot to order 2014 values from least to greatest
   plot.cols = source.cols
   
-  line_elec_netgen_1949_2014 = f.lineplot(dt, xval, yval, fillval, 
+  line_elec_netgen_1973_2017 = f.lineplot(dt, xval, yval, fillval, 
                                           tlab, sublab, xlab, ylab, leglab, gval, leg.ord, plot.cols) + 
-    scale_x_continuous(breaks = seq(1945,2015,10), expand = c(0,0)) +
-    scale_y_continuous(breaks = seq(0,2.5,0.5), expand = c(0,0), limits = c(0, 2.5))
+    scale_x_date(date_breaks = "5 years", date_labels = "%Y", expand = c(0,1)) +
+    scale_y_continuous(breaks = seq(0,200,20), expand = c(0,0), limits = c(0, 200))
   
-  ggsave(line_elec_netgen_1949_2014, 
+  ggsave(line_elec_netgen_1973_2017, 
          filename = "Electricity_8.2a_Electricity Net Gen Total All Sectors_LTS.png", 
          width = 11.1, 
          height = 6.25, 
          dpi = 400)
   
-# SEGMENT PLOT -------------
-  
-  dt = dt_annual[ ! MSN %in% c("Total")]
-  xval = dt[, YYYY]
-  yval = dt[, Value]/1000000
-  fillval = dt[, MSN]
-  wsize = 2
-  csize = 1
-  tlab = "1949 - 2014 Annual U.S. Electricity Generation By Source"
-  sublab = "Data: EIA Annual Energy Review"
-  gval = "Y"
-  xlab = NULL
-  ylab = "Kilowatt-hour (Trillions)"
-  leglab = ""
-  leg.ord = levels(with(dt[YYYY == "2014"], reorder(MSN, -Value)))
-  plot.cols = source.cols
-  
-  seg_elec_netgen_1949_2014 = f.segplot(dt, xval, yval, fillval, wsize, csize, 
-                                        tlab, sublab, xlab, ylab, leglab, gval, leg.ord, plot.cols) + 
-    scale_x_continuous(breaks = seq(1945,2015,10), expand = c(0,0)) +
-    scale_y_continuous(breaks = seq(0,2.5,0.5), expand = c(0,0), limits = c(0, 2.5))
-  
-  ggsave(seg_elec_netgen_1949_2014, 
-         filename = "Electricity_8.2a_Electricity Net Gen Total All Sectors_STS.png", 
-         width = 11.1, 
-         height = 6.25, 
-         dpi = 400)
