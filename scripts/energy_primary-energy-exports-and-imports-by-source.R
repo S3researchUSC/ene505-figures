a0ab705b188c97cec43d7981da29fdb25a043709
measmeng <measraim@usc.edu>
2020-08-22 20:24:55 -0700

    add total annual and monthly imports vs exports

--- scripts/energy_primary-energy-exports-and-imports-by-source.R
+++ scripts/energy_primary-energy-exports-and-imports-by-source.R
@@ -80,9 +80,7 @@ imp.file      = 'Table_1.4a_Primary_Energy_Imports_by_Source.xlsx'
   
   dt_month[ , (c('export', 'import')):= lapply(.SD, as.numeric), .SDcols = c('export', 'import')]
   dt_annual[ , (c('export', 'import')):= lapply(.SD, as.numeric), .SDcols = c('export', 'import')]
-  # dt_month[, value := as.numeric(value)]
-  # dt_annual[, value := as.numeric(value)]
-  
+
 # remove NA month or year entries ----
   
   dt_month = dt_month[!is.na(month)]
@@ -93,10 +91,20 @@ imp.file      = 'Table_1.4a_Primary_Energy_Imports_by_Source.xlsx'
   dt_annual_tot = dt_annual[ fuel %like% 'Total']
   dt_annual = dt_annual[ ! fuel %like% 'Total']
   
+  dt_month_tot = dt_month[ fuel %like% 'Total']
+  dt_month = dt_month[ ! fuel %like% 'Total']
+
 # longer data table ----
   
   dt_annual_long = melt(dt_annual[,1:4], measure.vars = colnames(dt_annual)[3:4],
                         variable.name = 'type', value.name = 'value')
+  
+  dt_annual_tot_long = melt(dt_annual_tot, measure.vars = colnames(dt_annual_tot)[3:4],
+                        variable.name = 'type', value.name = 'value')
+  
+  dt_month_tot_long = melt(dt_month_tot, measure.vars = colnames(dt_month_tot)[3:4],
+                            variable.name = 'type', value.name = 'value')
+  
   
 # calculate proportions -----
   
@@ -546,7 +554,7 @@ imp.file      = 'Table_1.4a_Primary_Energy_Imports_by_Source.xlsx'
       scale_x_continuous(breaks = seq(1949,2019,5), limits = c(1949, 2019), expand = c(0,0)) +
       scale_y_continuous(expand = c(0,0)) +
       scale_color_manual(values = pal_fuel) + 
-      scale_linetype_discrete(labels = c('Export', 'Import')) +
+      scale_linetype_discrete(labels = c('Exports', 'Imports')) +
       theme_line +
       theme(legend.text = element_text(size = 14),
             legend.position = 'bottom',
@@ -583,7 +591,7 @@ imp.file      = 'Table_1.4a_Primary_Energy_Imports_by_Source.xlsx'
       scale_x_continuous(breaks = seq(1949,2019,20), limits = c(1949, 2019), expand = c(0,0)) +
       scale_y_continuous(expand = c(0,0)) +
       scale_color_manual(values = pal_fuel) + 
-      scale_linetype_discrete(labels = c('Export', 'Import')) +
+      scale_linetype_discrete(labels = c('Exports', 'Imports')) +
       theme_line +
       theme(legend.text = element_text(size = 14),
             legend.position = 'bottom',
@@ -600,6 +608,78 @@ imp.file      = 'Table_1.4a_Primary_Energy_Imports_by_Source.xlsx'
     # save as png:
     # ggsave(fig_all_line_annual, 
     #        filename = here::here('figures', 'energy', 'primary-energy-exports-and-imports-by-source_annual_1949-2019_lts_faceted.png'), 
+    #        width = 11.5, 
+    #        height = 6.25, 
+    #        dpi = 600)
+    
+    
+  # exports-imports, line, annual, totals -------
+    
+    fig_all_line_annual_tot = ggplot(dt_annual_tot_long[fuel == 'Total Energy'], aes(x = year, y = value, linetype = type)) + 
+      geom_line(size = 1.1) +
+      labs(title = 'Annual U.S. total primary energy exports and imports (1949-2019)',
+           subtitle = 'Quadrillion BTU', 
+           caption = 'Data: U.S. Energy Information Administration',
+           x = NULL,
+           y = NULL,
+           color = NULL,
+           linetype = NULL) +
+      scale_x_continuous(breaks = seq(1949,2019,5), limits = c(1949, 2019), expand = c(0,0)) +
+      scale_y_continuous(expand = c(0,0)) +
+      # scale_color_manual(values = pal_type) + 
+      scale_linetype_discrete(labels = c('Exports', 'Imports')) +
+      theme_line +
+      theme(legend.text = element_text(size = 16),
+            legend.position = 'bottom',
+            plot.margin = unit(c(1,2,1,1), "lines"))
+    
+    ggsave(fig_all_line_annual_tot, 
+           filename = here::here('figures', 'energy', 'primary-energy-exports-and-imports-by-source_totals_annual_1949-2019_lts.pdf'), 
+           width = 11.5, 
+           height = 6.25)
+    
+    embed_fonts(here::here('figures', 'energy', 'primary-energy-exports-and-imports-by-source_totals_annual_1949-2019_lts.pdf'),
+                outfile = here::here('figures', 'energy', 'primary-energy-exports-and-imports-by-source_totals_annual_1949-2019_lts.pdf'))
+    
+    # save as png:
+    # ggsave(fig_all_line_annual_tot, 
+    #        filename = here::here('figures', 'energy', 'primary-energy-exports-and-imports-by-source_totals_annual_1949-2019_lts.png'), 
+    #        width = 11.5, 
+    #        height = 6.25, 
+    #        dpi = 600)
+    
+    
+    
+  # exports-imports, line, monthly, totals -------
+    
+    fig_all_line_month_tot = ggplot(dt_month_tot_long[fuel == 'Total Energy'], aes(x = month, y = value, linetype = type)) + 
+      geom_line(size = 0.6) +
+      labs(title = 'Monthly U.S. total primary energy exports and imports (1949-2019)',
+           subtitle = 'Quadrillion BTU', 
+           caption = 'Data: U.S. Energy Information Administration',
+           x = NULL,
+           y = NULL,
+           color = NULL,
+           linetype = NULL) +
+      scale_x_date(breaks = '5 years', date_labels = "%b %Y", expand = c(0,0)) +
+      scale_y_continuous(expand = c(0,0)) +
+      scale_linetype_discrete(labels = c('Exports', 'Imports')) +
+      theme_line +
+      theme(legend.text = element_text(size = 16),
+            legend.position = 'bottom',
+            plot.margin = unit(c(1,2,1,1), "lines"))
+    
+    ggsave(fig_all_line_month_tot, 
+           filename = here::here('figures', 'energy', 'primary-energy-exports-and-imports-by-source_totals_monthly_1949-2019_lts.pdf'), 
+           width = 11.5, 
+           height = 6.25)
+    
+    embed_fonts(here::here('figures', 'energy', 'primary-energy-exports-and-imports-by-source_totals_monthly_1949-2019_lts.pdf'),
+                outfile = here::here('figures', 'energy', 'primary-energy-exports-and-imports-by-source_totals_monthly_1949-2019_lts.pdf'))
+    
+    # save as png:
+    # ggsave(fig_all_line_month_tot, 
+    #        filename = here::here('figures', 'energy', 'primary-energy-exports-and-imports-by-source_totals_monthly_1949-2019_lts.png'), 
     #        width = 11.5, 
     #        height = 6.25, 
     #        dpi = 600)
