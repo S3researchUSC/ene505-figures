f089633cb33e34e5e4f16b1c4408827f5495fb58
measmeng <measraim@usc.edu>
2020-09-17 18:21:02 -0700

    line time series of gdp by country of top 10

--- scripts/world/gdp-by-country.R
+++ scripts/world/gdp-by-country.R
@@ -34,7 +34,7 @@
 # load data ------
   
   # read in csv (without API column)
-    gdp_data = fread(here::here('src', gdp_file), skip = 1, select = 2:42, header = T)
+    gdp_data = fread(here::here('data', gdp_file), skip = 1, select = 2:42, header = T)
   
   # rename first column name
     setnames(gdp_data, 'V2', 'country')
@@ -69,3 +69,42 @@
 # export ranked gdp in 2019 to csv file ----
   
   fwrite(gdp_most_recent, here::here('data', out_gdp_2019), row.names = F)
+  
+# ------------------------------------ figures ------------------------------------
+  
+  # line, annual -----
+    
+    labs_line = gdp_long[country %in% top_10_gdp[, country] & year == max(year)]
+    labs_line = labs_line[order(rank(gdp_billion_usd))]
+    labs_line[, position := gdp_billion_usd]
+    labs_line[1:7, position := seq(1200,7100,length.out = 7)]
+    # labs_line[is.na(position), position := value/1000]
+  
+    line_gdp = ggplot(gdp_long[country %in% top_10_gdp[, country]], aes(x = year, y = gdp_billion_usd, color = country)) + geom_line() +
+      geom_line(size = 0.9) +
+      labs(title = 'Annual gross domestic product (GDP) at purchasing power parities (1980-2019)',
+           subtitle = 'Billion 2015$ PPP', 
+           caption = 'Data: U.S. Energy Information Administration', 
+           x = NULL,
+           y = NULL) +
+      guides(color = 'none') +
+      scale_x_continuous(breaks = seq(1980,2019,5), limits = c(1980, 2019), expand = c(0,0)) +
+      scale_y_continuous(labels = scales::comma, expand = c(0,0)) +
+      scale_color_manual(values = pal_country) + 
+      theme_line +
+      geom_text(data = labs_line, aes(x = Inf, y = position, label = paste0(' ', country), color = country), hjust = 0,
+                size = 5.6, fontface = 'plain', family = 'Secca Soft') +
+      theme(plot.margin = unit(c(1,8,1,1), "lines"))
+  
+    line_gdp = ggplotGrob(line_gdp)
+    line_gdp$layout$clip[line_gdp$layout$name == "panel"] = "off"
+    
+    ggsave(line_gdp, 
+           filename = here::here('figures', 'world', 'gdp-by-country_annual_1980-2019_lts.pdf'), 
+           width = 11.5, 
+           height = 6.25)
+    
+    embed_fonts(here::here('figures', 'world', 'gdp-by-country_annual_1980-2019_lts.pdf'),
+                outfile = here::here('figures', 'world', 'gdp-by-country_annual_1980-2019_lts.pdf'))
+  
+    
