8c0de549d8888f9214f31cba3d9c15048b9e8089
measmeng <measraim@usc.edu>
2020-09-18 00:11:50 -0700

    plots of petroleum production globally

--- scripts/world/petroleum-by-country.R
+++ scripts/world/petroleum-by-country.R
@@ -108,4 +108,109 @@
   pal_exp = pal_5countries
   names(pal_exp)[2:6] = top5_exp
   
+  # line, production -----
+  
+    labs_line_prod = oil_agg[type == 'production' & year == 2019]
+    labs_line_prod = labs_line_prod[order(rank(value))]
+    labs_line_prod[, position := value/1e3]
+    labs_line_prod[1:4, position := c(3.9,6,10.6,12.4)]
+
+    line_prod = ggplot(oil_agg[type == 'production'], aes(x = year, y = value/1e3, color = label)) + 
+      geom_line(size = 0.9) +
+      labs(title = 'Annual petroleum production by country (1980-2019)',
+           subtitle = 'Million barrels per day', 
+           caption = 'Top 5 petroleum producing countries in 2019 shown individually. All other countries aggregated. Data: U.S. Energy Information Administration', 
+           x = NULL,
+           y = NULL) +
+      guides(color = 'none') +
+      scale_x_continuous(breaks = seq(1980,2019,5), limits = c(1980,2019), expand = c(0,0)) +
+      scale_y_continuous(labels = scales::comma, expand = c(0,0), breaks = seq(0,50,10), limits = c(0,50)) +
+      scale_color_manual(values = pal_prod) + 
+      theme_line +
+      geom_text(data = labs_line_prod, aes(x = Inf, y = position, label = paste0(' ', label), color = label), hjust = 0,
+                size = 6.5, fontface = 'plain', family = 'Secca Soft')
+    
+    line_prod = ggplotGrob(line_prod)
+    line_prod$layout$clip[line_prod$layout$name == "panel"] = "off"
+    
+    ggsave(line_prod, 
+           filename = here::here('figures', 'world', 'petroleum-production-by-country_annual_1980-2019_lts.pdf'), 
+           width = 11.5, 
+           height = 6.25)
+    
+    embed_fonts(here::here('figures', 'world', 'petroleum-production-by-country_annual_1980-2019_lts.pdf'),
+                outfile = here::here('figures', 'world', 'petroleum-production-by-country_annual_1980-2019_lts.pdf'))
+  
+  # area, production (absolute) -----
+  
+    labs_area_prod = oil_agg[type == 'production' & year == 2019][order(factor(label, levels = rev(c('All Other Countries', rev(top5_prod)))))]
+    labs_area_prod[, cum_sum := cumsum(value/1e3)] 
+    labs_area_prod[, difference := diff(c(0,cum_sum))/2]
+    labs_area_prod[, position := cum_sum - difference]
+    
+    area_prod = ggplot(oil_agg[type == 'production'], 
+                      aes(x = year, y = value/1e3, fill = factor(label, levels = c('All Other Countries', rev(top5_prod))))) + 
+      geom_area() +
+      labs(title = 'Annual petroleum production by country (1980-2019)',
+           subtitle = 'Million barrels per day', 
+           caption = 'Top 5 petroleum producing countries in 2019 shown individually. All other countries aggregated. Data: U.S. Energy Information Administration', 
+           x = NULL,
+           y = NULL) +
+      scale_x_continuous(breaks = seq(1980,2019,5), limits = c(1980,2019), expand = c(0,0)) +
+      scale_y_continuous(labels = scales::comma, expand = c(0,0), breaks = seq(0,110,10), limits = c(0,110)) +
+      guides(fill = 'none',
+             color = 'none') +
+      scale_color_manual(values = pal_prod) + 
+      scale_fill_manual(values = pal_prod) + 
+      theme_area_labeled +
+      geom_text(data = labs_area_prod, aes(x = Inf, y = position, label = paste0(' ', label), color = label), 
+                hjust = 0, size = 6.5, fontface = 'plain', family = 'Secca Soft')
+    
+    area_prod = ggplotGrob(area_prod)
+    area_prod$layout$clip[area_prod$layout$name == "panel"] = "off"
+    
+    ggsave(area_prod, 
+           filename = here::here('figures', 'world', 'petroleum-production-by-country_annual_1980-2019_ats_absolute.pdf'), 
+           width = 11.5, 
+           height = 6.25)
+    
+    embed_fonts(here::here('figures', 'world', 'petroleum-production-by-country_annual_1980-2019_ats_absolute.pdf'),
+                outfile = here::here('figures', 'world', 'petroleum-production-by-country_annual_1980-2019_ats_absolute.pdf'))
+  
+  
+  # area, production (proportion) -----
+  
+    labs_area_prod_prop = oil_agg[type == 'production' & year == 2019][order(factor(label, levels = rev(c('All Other Countries', rev(top5_prod)))))]
+    labs_area_prod_prop[, cum_sum := cumsum(prop)] 
+    labs_area_prod_prop[, difference := diff(c(0,cum_sum))/2]
+    labs_area_prod_prop[, position := cum_sum - difference]
+    
+    area_prod_prop = ggplot(oil_agg[type == 'production'], 
+                           aes(x = year, y = prop, fill = factor(label, levels = c('All Other Countries', rev(top5_prod))))) + 
+      geom_area() +
+      labs(title = 'Annual petroleum production by country (1980-2019)',
+           subtitle = 'Share of global petroleum production', 
+           caption = 'Top 5 petroleum producing countries in 2019 shown individually. All other countries aggregated. Data: U.S. Energy Information Administration', 
+           x = NULL,
+           y = NULL) +
+      scale_x_continuous(breaks = seq(1980,2019,5), limits = c(1980,2019), expand = c(0,0)) +
+      scale_y_continuous(labels = scales::percent, expand = c(0,0)) +
+      guides(fill = 'none',
+             color = 'none') +
+      scale_color_manual(values = pal_prod) + 
+      scale_fill_manual(values = pal_prod) + 
+      theme_area_labeled +
+      geom_text(data = labs_area_prod_prop, aes(x = Inf, y = position, label = paste0(' ', label), color = label), 
+                hjust = 0, size = 6.5, fontface = 'plain', family = 'Secca Soft')
+    
+    area_prod_prop = ggplotGrob(area_prod_prop)
+    area_prod_prop$layout$clip[area_prod_prop$layout$name == "panel"] = "off"
+    
+    ggsave(area_prod_prop, 
+           filename = here::here('figures', 'world', 'petroleum-production-by-country_annual_1980-2019_ats_proportion.pdf'), 
+           width = 11.5, 
+           height = 6.25)
+    
+    embed_fonts(here::here('figures', 'world', 'petroleum-production-by-country_annual_1980-2019_ats_proportion.pdf'),
+                outfile = here::here('figures', 'world', 'petroleum-production-by-country_annual_1980-2019_ats_proportion.pdf'))
   
