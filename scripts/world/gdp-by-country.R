520be41946aa3834dd613784e5ce4d0d7de67449
measmeng <measraim@usc.edu>
2020-09-17 17:05:31 -0700

    export ranked gdp by country in 2019 as csv file

--- scripts/world/gdp-by-country.R
+++ scripts/world/gdp-by-country.R
@@ -9,6 +9,12 @@
 
   gdp_file      = "international-gdp-by-country-1980-2019.csv"
 
+# ------------------------------------ outputs ------------------------------------
+  
+  out_gdp_2019  = "gdp-by-country-ranked-2019.csv"
+  
+# ------------------------------------ main script ------------------------------------
+  
 # load libraries -------
 
   library(data.table)
@@ -22,16 +28,16 @@
 
 # source color palettes and plot themes ------
 
-  items = list.files(here::here("src"))
-  sapply(here::here("src", items), source)
+  items = list.files(here::here('src'))
+  sapply(here::here('src', items), source)
   
 # load data ------
   
   # read in csv (without API column)
-    gdp_data = fread(here::here("data", gdp_file), skip = 1, select = 2:42, header = T)
+    gdp_data = fread(here::here('src', gdp_file), skip = 1, select = 2:42, header = T)
   
   # rename first column name
-    setnames(gdp_data, "V2", "country")
+    setnames(gdp_data, 'V2', 'country')
 
   # extract first row as unit
     unit = gdp_data[1, country]
@@ -40,20 +46,26 @@
     gdp_data = gdp_data[ ! country == unit ]
     
   # remove whitespace from country names
-    gdp_data[, country := trimws(country, which = "both")]
+    gdp_data[, country := trimws(country, which = 'both')]
     
 # melt data from wide to long format -----
     
   gdp_long = melt(gdp_data, measure.vars = as.character(1980:2019),
-                  variable.name = "year", value.name = "gdp_billion_usd")
+                  variable.name = 'year', value.name = 'gdp_billion_usd')
   
+# convert year and gdp values to numeric type -----
+    
   gdp_long[, year := as.numeric(as.character(year))]
   gdp_long[, gdp_billion_usd := as.numeric(gdp_billion_usd)]
   
-# get top ten GDP in latest year ----
+# get top ten GDP in latest year -----
   
-  gdp_most_recent = gdp_long[year == max(year) & ! country == "World"]
-  setorder(gdp_most_recent, -"gdp_billion_usd", na.last = T)
+  gdp_most_recent = gdp_long[year == max(year) & ! country == 'World']
+  setorder(gdp_most_recent, -'gdp_billion_usd', na.last = T)
   gdp_most_recent[, rank := 1:nrow(gdp_most_recent)]
   
   top_10_gdp = gdp_most_recent[rank %in% 1:10]
+  
+# export ranked gdp in 2019 to csv file ----
+  
+  fwrite(gdp_most_recent, here::here('data', out_gdp_2019), row.names = )
